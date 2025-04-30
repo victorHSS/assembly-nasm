881d7bc22c410061410028705f4529e7b226bfc4
Victor A. P. Oliveira <victorandreoliveira@gmail.com>
2025-04-29 23:21:26 -0300

    update hello

--- hello.asm
+++ hello.asm
@@ -1,16 +1,17 @@
 global _start
 
 section .data
-	message: db 'Hello, world!",10
+	message: db 'Hello, world!',10
 
 section .text
+_start:
 	mov rax, 1				; chamada ao sistema - função write
 	mov rdi, 1				; arg #1 - 1 -> stdout
-	mov rs1, message		; arg #2 - incício da string
+	mov rsi, message		; arg #2 - incício da string
 	mov rdx, 14				; arg #3 - tamanho da string
-	syscal
+	syscall
 
-exit:
+.exit:
 	mov rax, 60				; chamada ao sistema - função exit
 	xor rdi, rdi			; arg #1 - 0
 	syscall
